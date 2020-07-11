41a7a8409106cbcafba4b83eaebec33f1fdfe1ad
Taiwonator <taiwonator77@gmail.com>
2020-07-11 17:53:23 +0100

    navbar basic visual complete

--- src/components/ToggleButton/ToggleButton.js
+++ src/components/ToggleButton/ToggleButton.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import './ToggleButton.scss';
 
 const ToggleButton = (props) => (
-    <div/>
+    <div className="toggle-button-container">
+        <svg id="toggle-button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64.47 58.31">
+            <rect fill={props.color} x="990.89" y="471.22" width="3.44" height="64.29" transform="translate(-484.43 987.87) rotate(-89.23)" />
+            <rect fill={props.color} x="990.82" y="498.22" width="3.44" height="64.29" transform="translate(-511.49 1014.44) rotate(-89.23)" />
+            <rect fill={props.color} x="990.75" y="525.22" width="3.44" height="64.29" transform="translate(-538.56 1041) rotate(-89.23)" />
+        </svg>
+    </div>
 )
 
 export default ToggleButton;
--- src/containers/Navbar/Navbar.js
+++ src/containers/Navbar/Navbar.js
@@ -4,18 +4,23 @@ import './Navbar.scss';
 import CompanyTextLogo from '../../components/CompanyTextLogo/CompanyTextLogo.js';
 import ToggleButton from '../../components/ToggleButton/ToggleButton.js';
 
-function Navbar(props) {
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
     return (
         <nav className='navbar-container'>
             <div className='navbar-content'>
-                <CompanyTextLogo colors={props.colors}/>
-                <ToggleButton color={props.colors.blue}/>
+                <CompanyTextLogo colors={this.props.colors}/>
+                <ToggleButton color={this.props.colors.blue}/>
             </div>
             <ul>
 
             </ul>
         </nav>
-    )
+    )}
 }
 
 export default Navbar;
