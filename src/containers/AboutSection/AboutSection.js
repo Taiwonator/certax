747b195c616e86d9dc3a2966fd5b1a0373a255ec
Taiwonator <taiwonator77@gmail.com>
2020-07-13 13:09:41 +0100

    inital

--- src/containers/AboutSection/AboutSection.js
+++ src/containers/AboutSection/AboutSection.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 
 function AboutSection(props) {
     return (
-        <div>Hello Jim</div>
+        <div className='about-section-container'>
+
+        </div>
     )
 }
 
 export default AboutSection;
+
+// Graphics
+// Box
+// --Graphics
+// --Textbox
+// ----Graphics
+// ----Text Content
+// ------Header
+// ------Text
+// ------Button
