d65a9943e837afd308a6df807569db292e7cb09b
Taiwonator <taiwonator77@gmail.com>
2020-07-14 15:32:26 +0100

    Split content component creation

--- src/containers/InfoSection/InfoSection.js
+++ src/containers/InfoSection/InfoSection.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './InfoSection.scss';
+import ContentSplit from '../../components/ContentSplit/ContentSplit';
 
 function InfoSection(props) {
-    console.log(props.data);
     return (
         <div className='info-section-container'>
-
+            <div className='info-section-content-wrapper'>
+                <div className='left-side-graphic' />
+                <ContentSplit colors={props.colors} content={props.data}/>
+                <div className='empty-space' />
+            </div>
         </div>
     )
 }
