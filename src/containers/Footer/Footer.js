82d572ecba9d6f69662844c7097c0a7c700aa7e8
Taiwonator <taiwonator77@gmail.com>
2020-07-25 16:31:54 +0100

    initial

---
+++ src/containers/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Footer.scss';
+
+const Footer = (props) => {
+    return (
+        <div className='footer-container'>
+            
+        </div>
+    );
+}
+ 
+export default Footer;
