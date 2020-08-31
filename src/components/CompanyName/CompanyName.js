782bba3c99d3fc14fe94da20b5e76e659f59b21e
Taiwonator <taiwonator77@gmail.com>
2020-09-01 00:16:03 +0100

    Changed logo yellow text font and new line

--- src/components/CompanyName/CompanyName.js
+++ src/components/CompanyName/CompanyName.js
@@ -5,7 +5,7 @@ const CompanyName = (props) => (
     <div className="company-name-container">
         <h1 style={{color: props.colors.blue}}>CERTAX</h1>
         <h2 style={{color: props.colors.lightblue}}><span>accounting</span></h2>
-        <h3 style={{color: props.colors.yellow}}>{`Professional Accountants & Tax Advisers`}</h3>
+        <h3 style={{color: props.colors.yellow}}>Professional Accountants<br/> & Tax Advisers</h3>
     </div>
 );
 
