466bb9eee88a42e3576bcd714a0783a4b727663c
Taiwonator <taiwonator77@gmail.com>
2020-09-01 19:57:15 +0100

    intial

---
+++ src/containers/Chatbox/Chatbox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Chatbox.scss';
+
+class Chatbox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div> This is a chatbox </div> );
+    }
+}
+ 
+export default Chatbox;
