aaee2f8d62f6d257e6c16d468d598f5e10694b0a
Taiwonator <taiwonator77@gmail.com>
2020-09-02 13:15:41 +0100

    testing getABatch again

--- src/containers/GetAQuote/GetAQuote.js
+++ src/containers/GetAQuote/GetAQuote.js
@@ -24,8 +24,8 @@ class GetAQuote extends Component {
 
     getNewBatch = () => {
         this.lockAnswers();
-        // const batchObj = getABatch(this.state.answers, this.state.questions, this.state.batch + 1);
-        const batchObj = getBatch(this.state.answers, this.state.batch + 1);
+        const batchObj = getABatch(this.state.answers, this.state.questions, this.state.batch + 1);
+        // const batchObj = getBatch(this.state.answers, this.state.batch + 1);
         let keys = batchObj.newQuestions.map(a => a.key);
         let answersObj = this.state.answers;
         for(var i = 0; i < keys.length; i++) {
